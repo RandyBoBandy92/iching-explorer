fe4f8cb676cf3aea213b32411276bd471cd93c4e
Randy Gulak <randy@randygulak.com>
2024-05-31 08:09:16 -0700

    added hide show all, and added system to determine auspiciousness of each line

--- src/components/HexReading/HexReading.jsx
+++ src/components/HexReading/HexReading.jsx
@@ -8,17 +8,25 @@ import {
   getCorrectLineEnergy,
   getLineCorrelate,
 } from "../../utilities/toolbelt";
+import { useRef } from "react";
+
 function HexReading({ hexText, show, type }) {
   // putting a comment here because two functions side by side makes me autism
 
   const { lines, transformedLines } = useContext(GlobalContext);
 
-  const [lineText, setLineText] = useState([]);
+  const [options, setOptions] = useState({
+    onlyChanging: false,
+    showHideAll: true,
+  });
 
-  useEffect(() => {
+  const showAllRef = useRef();
+
+  function getCombinedLineData() {
     const tempLineText = [];
     for (let lineNumber = 1; lineNumber < 7; lineNumber++) {
-      const tempLineData = hexText[`line_${lineNumber}`];
+      const tempLineData = {};
+      tempLineData.translations = hexText[`line_${lineNumber}`];
       tempLineData.data =
         type === "primary"
           ? lines[`line${lineNumber}`]
@@ -27,23 +35,26 @@ function HexReading({ hexText, show, type }) {
       tempLineData.lineEnergy = getCorrectLineEnergy(
         tempLineData.data.value,
         tempLineData.lineNum,
-        type === "primary" ? lines : transformedLines
+        lines
       );
       tempLineData.lineCorrelateMatch = getLineCorrelate(
         tempLineData.data,
         tempLineData.lineNum,
-        type === "primary" ? lines : transformedLines
+        lines
       );
       tempLineText.push(tempLineData);
     }
-    setLineText(tempLineText);
-  }, []);
-
-  console.log(lineText);
+    return tempLineText;
+  }
 
   useEffect(() => {
-    const allDetails = document.querySelectorAll("details");
+    const allDetails = document.querySelectorAll(
+      "details h2, details h3, details h4"
+    );
     function handleDetailClick(event) {
+      if (["h2", "h3", "h4"].includes(event.target.localName)) {
+        event.stopPropagation();
+      }
       const isOpen = this.open;
       if (!isOpen) {
         // opening
@@ -78,10 +89,10 @@ function HexReading({ hexText, show, type }) {
             <h3>{heading}</h3>
           </summary>
           <div className="translations">
-            {text.map((textObj) => (
+            {text.map((textObj, index) => (
               <details
                 className="translation-option"
-                key={`${type}-${textObj.author}`}
+                key={`${index} ${textObj.translationText}`}
               >
                 <summary>
                   <h4>{textObj.author}</h4>
@@ -99,7 +110,7 @@ function HexReading({ hexText, show, type }) {
     const notesArray = notes.split("\n");
     return (
       <>
-        <div className="other-notes">
+        <div className="sub-reading other-notes">
           <details className="sub-reading notes">
             <summary>
               <h3>Notes</h3>
@@ -115,13 +126,143 @@ function HexReading({ hexText, show, type }) {
     );
   }
 
+  function renderLines() {
+    const linesDataCombined = getCombinedLineData();
+
+    const lineHtml = linesDataCombined.map((lineDataCombined) => {
+      // pull data from line data combined
+      const { lineNum, data, translations, lineEnergy, lineCorrelateMatch } =
+        lineDataCombined;
+
+      // determine auspiciousness of reading
+      // say that if the energy is good.. that's moderate
+      // if correlate is good, that's auspicious
+      // if both, very auspicious
+
+      let auspiciousness;
+      if (lineEnergy === "correct") {
+        auspiciousness = "Moderately Auspicious or Perhaps Bad idk";
+      }
+
+      if (lineCorrelateMatch) {
+        auspiciousness = "Highly Auspicious";
+      }
+
+      if (lineEnergy === "correct" && lineCorrelateMatch) {
+        auspiciousness = "Very Highly Auspicious";
+      }
+
+      if (lineEnergy !== "correct" && !lineCorrelateMatch) {
+        auspiciousness = "Not good lol";
+      }
+
+      const lineNumInt = Number(lineNum.slice(-1));
+
+      const showLineCondition =
+        (options.onlyChanging && data.changing) || !options.onlyChanging;
+      const showLine = showLineCondition ? true : false;
+
+      const translationLineText = [];
+
+      for (const author in translations) {
+        if (Object.hasOwnProperty.call(translations, author)) {
+          const translationText = translations[author];
+          translationLineText.push({ author, translationText });
+        }
+      }
+
+      return (
+        <>
+          <details
+            className={`changing-line ${lineNum} ${showLine ? "show" : "hide"}`}
+          >
+            <summary>
+              <h4>Line {lineNumInt}</h4>
+            </summary>
+            <div className="changing-line-contents">
+              <h4>Line Energy: {lineEnergy}</h4>
+              <h4>
+                Line Correlate Match{" "}
+                {lineCorrelateMatch ? "Correlated" : "Uncorrelated"}
+              </h4>
+              <h4>Expected Outcome: {auspiciousness}</h4>
+
+              <div className="line-translations">
+                {translationLineText.map((translationLineText) => (
+                  <>
+                    <details open className="changing-line-option ">
+                      <summary>{translationLineText.author}</summary>
+                      <p>{translationLineText.translationText}</p>
+                    </details>
+                  </>
+                ))}
+              </div>
+            </div>
+          </details>
+        </>
+      );
+    });
+    return (
+      <>
+        <div className="sub-reading changing-lines">
+          <details>
+            <summary>
+              <h3>Changing Lines</h3>
+            </summary>
+            {lineHtml}
+          </details>
+        </div>
+      </>
+    );
+  }
+
+  function handleToggleShowAll() {
+    const buttonElem = showAllRef.current;
+    const mainDetails = buttonElem.parentElement.parentElement;
+    const allSubDetails = mainDetails.querySelectorAll("details");
+    const shouldShow = options.showHideAll;
+    if (!mainDetails.open) {
+      mainDetails.open = true;
+    }
+    // if (shouldShow) {
+    //   mainDetails.open = true;
+    // } else {
+    //   mainDetails.removeAttribute("open");
+    // }
+    allSubDetails.forEach((detail) => {
+      if (shouldShow) {
+        detail.open = true;
+      } else {
+        detail.removeAttribute("open");
+      }
+    });
+    setOptions({ ...options, showHideAll: !options.showHideAll });
+  }
+
+  console.log(options);
+
   return (
     <details className={`hex-reading ${show ? "show" : "hide"}`}>
       <summary>
         <h2>
           Hexgram {hexText.number} | {hexText.title}
         </h2>
+        <button ref={showAllRef} onClick={handleToggleShowAll}>
+          Show/Hide All
+        </button>
       </summary>
+      <div className="options">
+        <label htmlFor="options-lines">Only show changing</label>
+        <input
+          type="checkbox"
+          name="options-lines"
+          id="options-lines"
+          onChange={() => {
+            setOptions({ ...options, onlyChanging: !options.onlyChanging });
+          }}
+          checked={options.onlyChanging}
+        />
+      </div>
       <div className="other-titles">
         <p>{hexText.other_titles}</p>
       </div>
@@ -129,6 +270,7 @@ function HexReading({ hexText, show, type }) {
         {renderCategory("Judgement", hexText.Judgment)}
         {renderCategory("Image", hexText.Image)}
         {renderNotes(hexText.Notes)}
+        {renderLines()}
       </div>
     </details>
   );
