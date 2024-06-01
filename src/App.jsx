19ba9e29fe303b468452e7b583df90a5912d933c
Randy Gulak <randy@randygulak.com>
2024-06-01 16:11:30 -0700

    refactored debug menu

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hexagram from "./components/Hexagram/Hexagram";
 import { useEffect } from "react";
 import { useRef } from "react";
 import HexReading from "./components/HexReading/HexReading";
+import DebugMenu from "./components/DebugMenu/DebugMenu";
 
 function App() {
   const {
@@ -27,31 +28,6 @@ function App() {
   const changeHexRef = useRef();
   const desiredHexRef = useRef();
 
-  function handleIncrementOrDecrement(increment) {
-    // if new value is going to be less than 0, set it to 0
-    // if new value is going to be more than 64, set it to 64
-    let newHexNum = +newHexagramNumber + increment;
-    if (newHexNum < 0) newHexNum = 0;
-    if (newHexNum > 64) newHexNum = 64;
-    setNewHexagramNumber(newHexNum);
-    forceChangeHexagram(newHexNum);
-  }
-
-  function handleHexSubmit(e) {
-    e.preventDefault();
-    forceChangeHexagram(+newHexagramNumber);
-    // blur the input
-    changeHexRef.current.blur();
-  }
-
-  function handleDesiredHexSubmit(e) {
-    e.preventDefault();
-    if (hexagram.number === 0) return;
-    setDesiredHexagram(+desiredHexagramNumber);
-    // blur the input
-    desiredHexRef.current.blur();
-  }
-
   function handleSearch(href) {
     const searchParams = decodeURIComponent(href.split("?search=")[1]);
     if (!searchParams || searchParams === "undefined") return;
@@ -69,18 +45,6 @@ function App() {
       forceChangeHexagram(+start);
       setNewHexagramNumber(+start);
     }
-  }
-
-  function specialHyperLink(e) {
-    e.preventDefault();
-
-    const href = e.target.href;
-    // get the search query
-    handleSearch(href);
-  }
-
-  function handleReset() {
-    location.href = location.href.split("?")[0];
   }
 
   useEffect(() => {
@@ -125,123 +89,16 @@ function App() {
         <Hexagram type="transformed" />
         {/* <Hexagram hexagramData={transformedHexagram} type="transformed" /> */}
       </div>
+      <DebugMenu
+        newHexagramNumber={newHexagramNumber}
+        setNewHexagramNumber={setNewHexagramNumber}
+        desiredHexagramNumber={desiredHexagramNumber}
+        setDesiredHexagramNumber={setDesiredHexagramNumber}
+        handleSearch={handleSearch}
+        changeHexRef={changeHexRef}
+        desiredHexRef={desiredHexRef}
+      />
 
-      {/* Begin DEBUG Menu */}
-      <div className="debug">
-        <hr />
-        <h2>debug menu</h2>
-        <form onSubmit={handleHexSubmit}>
-          <input
-            ref={changeHexRef}
-            type="number"
-            min={0}
-            max={64}
-            value={newHexagramNumber}
-            onChange={(e) => {
-              setNewHexagramNumber(e.target.value);
-            }}
-          />
-          <button type="submit">Change Hex</button>
-        </form>
-
-        <button onClick={() => handleIncrementOrDecrement(1)}>Increase</button>
-        <button onClick={() => handleIncrementOrDecrement(-1)}>
-          Decrement
-        </button>
-        <button onClick={handleReset}>Reset</button>
-        <form onSubmit={handleDesiredHexSubmit}>
-          <input
-            ref={desiredHexRef}
-            type="number"
-            disabled={hexagram.number === 0}
-            value={desiredHexagramNumber}
-            onChange={(e) => setDesiredHexagramNumber(e.target.value)}
-          />
-          <button disabled={hexagram.number === 0} type="submit">
-            Set Desired Hex
-          </button>
-        </form>
-        <button onClick={() => flipHexagram("primary")}>
-          Flip Primary Hexagram
-        </button>
-        <button onClick={() => flipHexagram("transformed")}>
-          Flip Transformed Hexagram
-        </button>
-        <div className="modifiers ">
-          <h3>Modifiers</h3>
-          <label htmlFor="random">Random?</label>
-          <input
-            type="checkbox"
-            name="random"
-            value={random}
-            onChange={() => setRandom(!random)}
-          />
-        </div>
-        <div className="special-links show">
-          <a onClick={specialHyperLink} href="?search=11=>16">
-            Peace to Enthusiasm
-          </a>
-          <hr />
-          <a onClick={specialHyperLink} href="?search=3=>63">
-            Difficulty to After Completion
-          </a>
-          <hr />
-          <a onClick={specialHyperLink} href="?search=56">
-            The Wanderer
-          </a>
-          <hr />
-          <a onClick={specialHyperLink} href="?search=50=>33">
-            Cauldron to Retreat
-          </a>
-          <hr />
-          <a onClick={specialHyperLink} href="?search=64=>43">
-            unfinished business to resoluteness
-          </a>
-          <hr />
-        </div>
-        <div className="months-debug hide">
-          <h3>Months</h3>
-          <div className="months">
-            <a onClick={specialHyperLink} href="?search=19=>11">
-              January
-            </a>
-            <a onClick={specialHyperLink} href="?search=11=>34">
-              February
-            </a>
-            <a onClick={specialHyperLink} href="?search=34=>43">
-              March
-            </a>
-            <a onClick={specialHyperLink} href="?search=43=>1">
-              April
-            </a>
-            <a onClick={specialHyperLink} href="?search=1=>44">
-              May
-            </a>
-            <a onClick={specialHyperLink} href="?search=44=>33">
-              June
-            </a>
-            <a onClick={specialHyperLink} href="?search=33=>12">
-              July
-            </a>
-            <a onClick={specialHyperLink} href="?search=12=>20">
-              August
-            </a>
-            <a onClick={specialHyperLink} href="?search=20=>23">
-              September
-            </a>
-            <a onClick={specialHyperLink} href="?search=23=>2">
-              October
-            </a>
-            <a onClick={specialHyperLink} href="?search=2=>24">
-              November
-            </a>
-            <a onClick={specialHyperLink} href="?search=24=>19">
-              December
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* END DEBUG Menu */}
       {primaryHexText && transformedHexText && (
         <>
           <div className="reading-select">
---
+++ src/components/DebugMenu/DebugMenu.jsx
@@ -0,0 +1,190 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalContext";
+import { useRef } from "react";
+
+function DebugMenu({
+  newHexagramNumber,
+  setNewHexagramNumber,
+  desiredHexagramNumber,
+  setDesiredHexagramNumber,
+  handleSearch,
+  changeHexRef,
+  desiredHexRef,
+}) {
+  const {
+    hexagram,
+    forceChangeHexagram,
+    flipHexagram,
+    random,
+    setRandom,
+    setDesiredHexagram,
+  } = useContext(GlobalContext);
+
+  function handleReset() {
+    location.href = location.href.split("?")[0];
+  }
+
+  function specialHyperLink(e) {
+    e.preventDefault();
+
+    const href = e.target.href;
+    // get the search query
+    handleSearch(href);
+  }
+  function handleHexSubmit(e) {
+    e.preventDefault();
+    forceChangeHexagram(+newHexagramNumber);
+    // blur the input
+    changeHexRef.current.blur();
+  }
+  function handleIncrementOrDecrement(increment) {
+    // if new value is going to be less than 0, set it to 0
+    // if new value is going to be more than 64, set it to 64
+    let newHexNum = +newHexagramNumber + increment;
+    if (newHexNum < 0) newHexNum = 0;
+    if (newHexNum > 64) newHexNum = 64;
+    setNewHexagramNumber(newHexNum);
+    forceChangeHexagram(newHexNum);
+  }
+  function handleDesiredHexSubmit(e) {
+    e.preventDefault();
+    if (hexagram.number === 0) return;
+    setDesiredHexagram(+desiredHexagramNumber);
+    // blur the input
+    desiredHexRef.current.blur();
+  }
+  return (
+    <>
+      {/* Begin DEBUG Menu */}
+      <div className="debug">
+        <hr />
+        <h2>debug menu</h2>
+        <form onSubmit={handleHexSubmit}>
+          <input
+            ref={changeHexRef}
+            type="number"
+            min={0}
+            max={64}
+            value={newHexagramNumber}
+            onChange={(e) => {
+              setNewHexagramNumber(e.target.value);
+            }}
+          />
+          <button type="submit">Change Hex</button>
+        </form>
+
+        <button onClick={() => handleIncrementOrDecrement(1)}>Increase</button>
+        <button onClick={() => handleIncrementOrDecrement(-1)}>
+          Decrement
+        </button>
+        <button onClick={handleReset}>Reset</button>
+        <form onSubmit={handleDesiredHexSubmit}>
+          <input
+            ref={desiredHexRef}
+            type="number"
+            disabled={hexagram.number === 0}
+            value={desiredHexagramNumber}
+            onChange={(e) => setDesiredHexagramNumber(e.target.value)}
+          />
+          <button disabled={hexagram.number === 0} type="submit">
+            Set Desired Hex
+          </button>
+        </form>
+        <button onClick={() => flipHexagram("primary")}>
+          Flip Primary Hexagram
+        </button>
+        <button onClick={() => flipHexagram("transformed")}>
+          Flip Transformed Hexagram
+        </button>
+        <div className="modifiers ">
+          <h3>Modifiers</h3>
+          <label htmlFor="random">Random?</label>
+          <input
+            type="checkbox"
+            name="random"
+            value={random}
+            onChange={() => setRandom(!random)}
+          />
+        </div>
+        <div className="special-links show">
+          <a onClick={specialHyperLink} href="?search=11=>16">
+            Peace to Enthusiasm
+          </a>
+          <hr />
+          <a onClick={specialHyperLink} href="?search=3=>63">
+            Difficulty to After Completion
+          </a>
+          <hr />
+          <a onClick={specialHyperLink} href="?search=56">
+            The Wanderer
+          </a>
+          <hr />
+          <a onClick={specialHyperLink} href="?search=50=>33">
+            Cauldron to Retreat
+          </a>
+          <hr />
+          <a onClick={specialHyperLink} href="?search=64=>43">
+            unfinished business to resoluteness
+          </a>
+          <hr />
+        </div>
+        <div className="months-debug hide">
+          <h3>Months</h3>
+          <div className="months">
+            <a onClick={specialHyperLink} href="?search=19=>11">
+              January
+            </a>
+            <a onClick={specialHyperLink} href="?search=11=>34">
+              February
+            </a>
+            <a onClick={specialHyperLink} href="?search=34=>43">
+              March
+            </a>
+            <a onClick={specialHyperLink} href="?search=43=>1">
+              April
+            </a>
+            <a onClick={specialHyperLink} href="?search=1=>44">
+              May
+            </a>
+            <a onClick={specialHyperLink} href="?search=44=>33">
+              June
+            </a>
+            <a onClick={specialHyperLink} href="?search=33=>12">
+              July
+            </a>
+            <a onClick={specialHyperLink} href="?search=12=>20">
+              August
+            </a>
+            <a onClick={specialHyperLink} href="?search=20=>23">
+              September
+            </a>
+            <a onClick={specialHyperLink} href="?search=23=>2">
+              October
+            </a>
+            <a onClick={specialHyperLink} href="?search=2=>24">
+              November
+            </a>
+            <a onClick={specialHyperLink} href="?search=24=>19">
+              December
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* END DEBUG Menu */}
+    </>
+  );
+}
+
+export default DebugMenu;
+
+import PropTypes from "prop-types";
+
+DebugMenu.propTypes = {
+  newHexagramNumber: PropTypes.number.isRequired,
+  setNewHexagramNumber: PropTypes.func.isRequired,
+  desiredHexagramNumber: PropTypes.number.isRequired,
+  setDesiredHexagramNumber: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  changeHexRef: PropTypes.object.isRequired,
+  desiredHexRef: PropTypes.object.isRequired,
+};
