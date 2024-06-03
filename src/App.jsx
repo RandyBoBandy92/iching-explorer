6b9371c6a06fb803f6fac82659b00b243567148c
Randy Gulak <randy@randygulak.com>
2024-06-03 13:52:27 -0700

    cleaned up App with useAppHooks

--- src/App.jsx
+++ src/App.jsx
@@ -7,74 +7,32 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import HexReading from "./components/HexReading/HexReading";
 import DebugMenu from "./components/DebugMenu/DebugMenu";
+import { useAppHooks } from "./hooks/useAppHooks";
 
 function App() {
   const {
-    hexagram,
     forceChangeHexagram,
     setDesiredHexagram,
     changingLinesExist,
     primaryHexText,
     transformedHexText,
   } = useContext(GlobalContext);
-  const [newHexagramNumber, setNewHexagramNumber] = useState(hexagram.number);
-  const [desiredHexagramNumber, setDesiredHexagramNumber] = useState(0);
-  const [loadingReading, setLoadingReading] = useState();
-  const [readingToShow, setReadingToShow] = useState("primary");
 
-  const changeHexRef = useRef();
-  const desiredHexRef = useRef();
-
-  function handleSearch(href) {
-    const searchParams = decodeURIComponent(href.split("?search=")[1]);
-    if (!searchParams || searchParams === "undefined") return;
-    const type = searchParams.includes("=>") ? "multi" : "single";
-    if (type === "single") {
-      // if it's a single hexagram
-      // change the hexagram
-      forceChangeHexagram(+searchParams);
-      setNewHexagramNumber(+searchParams);
-    } else {
-      // if it's a multi hexagram
-      // split the search query into start and end
-      const [start, end] = searchParams.split("=>");
-      setLoadingReading(+end);
-      forceChangeHexagram(+start);
-      setNewHexagramNumber(+start);
-    }
-  }
-
-  useEffect(() => {
-    if (loadingReading) {
-      setDesiredHexagram(loadingReading);
-      setDesiredHexagramNumber(loadingReading);
-      setLoadingReading();
-    }
-  }, [loadingReading]);
-
-  useEffect(() => {
-    const currentHref = window.location.href;
-    const searchParams = decodeURIComponent(currentHref.split("?search=")[1]);
-    if (searchParams) {
-      handleSearch(currentHref);
-    }
-    const handleKeyDown = (e) => {
-      // if key combo is shift + /, focus on the desired hex input
-      if (e.key === "?" && e.shiftKey) {
-        desiredHexRef.current.focus();
-      }
-
-      // if key down is '/' focus on the input
-      if (e.key === "/") {
-        changeHexRef.current.focus();
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+  // return { changeHexRef, desiredHexRef, handleSearch };
+  const {
+    handleSearch,
+    changeHexRef,
+    desiredHexRef,
+    newHexagramNumber,
+    setNewHexagramNumber,
+    desiredHexagramNumber,
+    setDesiredHexagramNumber,
+    readingToShow,
+    setReadingToShow,
+  } = useAppHooks({
+    forceChangeHexagram,
+    setDesiredHexagram,
+  });
 
   const showTransformClass = changingLinesExist ? "show-transform" : "";
 
