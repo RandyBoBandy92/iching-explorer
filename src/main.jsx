08274835f75f1b2a5bf12dfa7686875925ba4a34
Randy Gulak <randy@randygulak.com>
2024-06-03 14:35:11 -0700

    added react router and basic browser router

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import "./variables.css";
 import "./index.css";
 import "./utilities/utilities.css";
 import { GlobalProvider } from "./context/GlobalContext.jsx";
+import { RouterProvider } from "react-router-dom";
+import { browserRouter } from "./router/Router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalProvider>
-      <App />
+      <RouterProvider router={browserRouter} />
     </GlobalProvider>
   </React.StrictMode>
 );
