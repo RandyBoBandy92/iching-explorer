6ab89ce6e3504cafaf38c7d7afd24f0b564b71e2
Randy Gulak <randy@randygulak.com>
2024-07-01 10:14:51 -0700

    added 404 page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import BottomNav from "../components/Nav/Nav";
 import Home from "../pages/home/Home";
 import Settings from "../pages/settings/Settings";
 import JournalPage from "../pages/journal/JournalPage";
+import PageNotFound from "../pages/404/PageNotFound";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/journal" element={<JournalPage />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <BottomNav />
     </Router>
