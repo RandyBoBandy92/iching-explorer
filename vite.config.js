a1fc64cf9a533ef5a25bf4c0df895c79b5bd0c08
Randy Gulak <randy@randygulak.com>
2024-10-09 22:53:02 -0700

    added service worker?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sw: resolve(__dirname, "public/service-worker.js"),
+      },
+    },
   },
 });
